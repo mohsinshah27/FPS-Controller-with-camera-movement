f336135299dcd5ea403704b7e9d06b2ad4aee64a
Mohsin Hassan Shah <93952536+mohsinshah27@users.noreply.github.com>
2021-11-09 04:07:15 +0500

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSen = 100f;
+
+    public Transform PlayerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSen * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")* mouseSen * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
